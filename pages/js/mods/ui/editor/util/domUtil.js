47e5d5e61cdddaaaeb8579968029452f7d0f2d98
JiLong <skyujilong@163.com>
2017-01-16 17:22:04 +0800

    add jquery like

--- pages/js/mods/ui/editor/util/domUtil.js
+++ pages/js/mods/ui/editor/util/domUtil.js
@@ -2,57 +2,89 @@
 var _ = require('lodash/core');
 var util = require('./util.js');
 var qwery = require('qwery');
+//通过DOMUtil DOMUtil.fn.init DOMUtil.prototype 将prototype赋值给
+function DOMUtil(selector, context) {
+    return new DOMUtil.fn.init(selector, context);
+}
+DOMUtil.fn = DOMUtil.prototype = {
 
+    constructor: DOMUtil,
 
-var domUtil = {
-    //NODE类型
-    NODE_TYPE:{
-        ELEMENT_NODE : 1,
-        TEXT_NODE : 3,
-        COMMENT_NODE : 8,
-        DOCUMENT_NODE : 9,
-        DOCUMENT_TYPE_NODE : 10,
-        DOCUMENT_FRAGMENT_NODE : 11
-    },
-    /**
-     * 选择器
-     * @param  {String} selector 选择器
-     * @param  {DOM} dom      dom范围
-     * @return {DOM|Array<DOM>}  返回选中的dom
-     */
-    $: function(selector, dom) {
-        var idReg = /#[^\.>\s]+$/,
-            isIdFlag = false;
+    init: function(selector, context) {
+        var eleList;
+        this.selector = selector;
         if (!_.isString(selector)) {
             throw new Error('need a string');
         }
         selector = util.trim(selector);
 
-        isIdFlag = idReg.test(selector);
-
-        if (!dom) {
-            dom = window.document;
+        if (!context) {
+            context = window.document;
         }
-        if (isIdFlag) {
-            return qwery(selector, dom)[0];
-        } else {
-            return qwery(selector, dom);
-        }
-    },
-    text: function(node, text) {
-        var val;
-        _.each(this.getChildren(node),function(node){
-            if(node.nodeType === this.NODE_TYPE.TEXT_NODE && text){
-                node.data = text;
-            }else if(node.nodeType === this.NODE_TYPE.TEXT_NODE && !text){
-                val = node.data;
-            }
+        eleList = qwery(selector, context);
+        this.length = eleList.length;
+        _.forEach(eleList,function(ele,index){
+            this[index] = ele;
         }.bind(this));
-        return val || node;
-    },
-    getChildren: function(node){
-        return node.childNodes;
     }
+}
+DOMUtil.fn.init.prototype = DOMUtil.fn;
 
-};
-module.exports = domUtil;
+// var domUtil = {
+//     //NODE类型
+//     NODE_TYPE: {
+//         ELEMENT_NODE: 1,
+//         TEXT_NODE: 3,
+//         COMMENT_NODE: 8,
+//         DOCUMENT_NODE: 9,
+//         DOCUMENT_TYPE_NODE: 10,
+//         DOCUMENT_FRAGMENT_NODE: 11
+//     },
+//     /**
+//      * 选择器
+//      * @param  {String} selector 选择器
+//      * @param  {DOM} dom      dom范围
+//      * @return {DOM|Array<DOM>}  返回选中的dom
+//      */
+//     $: function(selector, dom) {
+//         var idReg = /#[^\.>\s]+$/,
+//             isIdFlag = false;
+//         if (!_.isString(selector)) {
+//             throw new Error('need a string');
+//         }
+//         selector = util.trim(selector);
+//
+//         isIdFlag = idReg.test(selector);
+//
+//         if (!dom) {
+//             dom = window.document;
+//         }
+//         if (isIdFlag) {
+//             return qwery(selector, dom)[0];
+//         } else {
+//             return qwery(selector, dom);
+//         }
+//     },
+//     text: function(node, text) {
+//         var val;
+//         _.each(node.childNodes, function(node) {
+//             if (node.nodeType === this.NODE_TYPE.TEXT_NODE && text) {
+//                 node.data = text;
+//             } else if (node.nodeType === this.NODE_TYPE.TEXT_NODE && !text) {
+//                 val = node.data;
+//             }
+//         }.bind(this));
+//         return val || node;
+//     },
+//     html: function(node, text) {
+//         var flag = node.nodeType === this.NODE_TYPE.ELEMENT_NODE;
+//         if (flag && text) {
+//             node.innerHTML = text;
+//             return node;
+//         } else if (flag && !text) {
+//             return node.innerHTML;
+//         }
+//     }
+//
+// };
+module.exports = DOMUtil;
--- pages/js/page/domTest.js
+++ pages/js/page/domTest.js
@@ -1,16 +1,19 @@
 'use strict';
-var domUtil = require('../mods/ui/editor/util/domUtil');
-if(!console || !console.log){
-    window.console = {
-        log: function(text){
-            alert(text);
-        }
-    }
-}
-var d1Dom = domUtil.$('#d1');
-console.log(d1Dom);
-var d2Dom = domUtil.$('#d2');
-console.log(d2Dom);
-
-console.log(domUtil.$('span')[0].childNodes);
-domUtil.text(domUtil.$('span')[0],'wo cao zheshi ceshi');
+// var domUtil = require('../mods/ui/editor/util/domUtil');
+// if(!console || !console.log){
+//     window.console = {
+//         log: function(text){
+//             alert(text);
+//         }
+//     }
+// }
+// var d1Dom = domUtil.$('#d1');
+// console.log(d1Dom);
+// var d2Dom = domUtil.$('#d2');
+// console.log(d2Dom);
+//
+// console.log(domUtil.$('span')[0].childNodes);
+// domUtil.text(domUtil.$('span')[0],'wo cao zheshi ceshi!!!!');
+// domUtil.html(d1Dom,'<h1>我了个去</h1>');
+var $ = require('../mods/ui/editor/util/domUtil');
+console.log($("div"));
